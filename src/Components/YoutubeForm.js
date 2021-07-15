84546277cd2a9caf7bd2ff94f2126ddd41f643c6
Sarwar-Putul <sarwarputul@gmail.com>
2021-07-16 00:55:43 +0600

    tutorial 1 to 16

---
+++ src/Components/YoutubeForm.js
@@ -0,0 +1,52 @@
+import { useFormik } from 'formik';
+import React from 'react';
+import * as Yup from 'yup';
+
+const YoutubeForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            channel: ''
+        },
+        onSubmit: values => {
+            console.log('form', values)
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().required('Required'),
+            email: Yup.string().email('Invalid email address').required('Required'),
+            channel: Yup.string().required('Required')
+        })
+        
+    })
+    // console.log('form values', formik.values)
+    return (
+        <div className="form">
+            <form onSubmit={formik.handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" {...formik.getFieldProps('name')}  />
+                    
+                    {formik.touched.name && formik.errors.name ? (<div style= {{color: 'red'}}>{formik.errors.name}</div>) : null}
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">email</label>
+                    <input type="email" id="email" name="email" {...formik.getFieldProps('email')}  />
+                    
+                    {formik.touched.email && formik.errors.email ? (<div style= {{color: 'red'}}>{formik.errors.email}</div>) : null}
+                </div>
+                <div className="form-control">
+                    <label htmlFor="channel">channel</label>
+                    <input type="text" id="channel" name="channel" {...formik.getFieldProps('channel')}  />
+                    
+                    {formik.touched.channel && formik.errors.channel ? (<div style= {{color: 'red'}}>{formik.errors.channel}</div>) : null}
+                </div>    
+            
+                <button type="submit">Submit</button>
+            
+            </form>
+        </div>
+    );
+};
+
+export default YoutubeForm;
